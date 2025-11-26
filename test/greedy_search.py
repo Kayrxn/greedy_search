714c23dfaeba0768d1aa6c3c2c3233c9cade65ba
Kayrxn <xharukax99@gmail.com>
2025-11-26 14:00:55 +0100

    test: Añadir casos tests

---
+++ test/greedy_search.py
@@ -0,0 +1,13 @@
+import pytest
+from src.greedy import greedy_search, needed_states, stations
+
+@pytest.mark.greedy_basic
+def test_greedy_basic():  #hay alguna estación cubriendo algún estado
+    selected, covered = greedy_search(needed_states, stations)
+    assert len(selected) > 0
+    assert len(covered) > 0
+
+@pytest.mark.greedy_full
+def test_greedy_full_coverage():  #todas las estaciones están cubiertas
+    selected, covered = greedy_search(needed_states, stations)
+    assert covered == needed_states
