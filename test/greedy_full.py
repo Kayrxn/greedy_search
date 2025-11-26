a0613c962ae00e21b6848b0de77acb80c9c501db
Kayrxn <xharukax99@gmail.com>
2025-11-26 14:19:10 +0100

    refactor: Refactorizar tests

--- test/greedy_full.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from src.greedy import greedy_search, needed_states, stations
-
-@pytest.mark.greedy
-def test_greedy_full_coverage():  #todas las estaciones están cubiertas
-    selected, covered = greedy_search(needed_states, stations)
-    assert covered == needed_states
--- test/test_greedy.py
+++ test/test_greedy.py
@@ -6,3 +6,8 @@ def test_greedy_basic():  #hay alguna estación cubriendo algún estado
     selected, covered = greedy_search(needed_states, stations)
     assert len(selected) > 0
     assert len(covered) > 0
+
+@pytest.mark.greedy
+def test_greedy_full_coverage():  #todas las estaciones están cubiertas
+    selected, covered = greedy_search(needed_states, stations)
+    assert covered == needed_states
